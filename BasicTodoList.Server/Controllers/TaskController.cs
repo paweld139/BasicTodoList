9b2afe450912324654c984632743f75a5eec7fe3
paweld139 <pawell139139@gmail.com>
2024-05-13 19:37:41 +0200

    Dev

--- BasicTodoList.Server/Controllers/TaskController.cs
+++ BasicTodoList.Server/Controllers/TaskController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BasicTodoList.Server.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class TaskController(ITaskService taskService) : ControllerBase
     {
         public async Task<IActionResult> Get()
--- basictodolist.client/src/App.tsx
+++ basictodolist.client/src/App.tsx
@@ -1,38 +1,33 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
+interface Task {
+    id: number;
+    title: string;
+    isCompleted: boolean;
 }
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+    const [tasks, setTasks] = useState<Task[]>();
 
     useEffect(() => {
-        populateWeatherData();
+        populateTaskData();
     }, []);
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
+    const contents = tasks === undefined
+        ? <p><em>Loading...</em></p>
+        : <table className="table table-striped" aria-labelledby="tableLabel">
             <thead>
                 <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
+                    <th>Title</th>
+                    <th>Is completed</th>
                 </tr>
             </thead>
             <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
+                {tasks.map(task =>
+                    <tr key={task.id}>
+                        <td>{task.title}</td>
+                        <td>{task.isCompleted ? 'Yes' : 'No'}</td>
                     </tr>
                 )}
             </tbody>
@@ -40,16 +35,16 @@ function App() {
 
     return (
         <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
+            <h1 id="tableLabel">Tasks</h1>
+            <p>This table shows the tasks.</p>
             {contents}
         </div>
     );
 
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
+    async function populateTaskData() {
+        const response = await fetch('/api/task');
         const data = await response.json();
-        setForecasts(data);
+        setTasks(data);
     }
 }
 
